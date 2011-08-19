8a533a521d2e718bcb7fde5876a66d4fe086ec16
Eugene Howe <ehowe@engineyard.com>
2011-08-19 10:39:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyWordpress::Application.config.secret_token = '74cb51326330cb326290b8fde1d9d4d8d5ebf44c5fcf352133f332b57ceb09bed23314450f513c2be45f563aa1117e5a57975b917d90ccde6266430a0c460a9f'
